3ed8486f228687f402430d16a5d0566f39253a3a
ijs32 <iansilber32@gmail.com>
2022-09-20 22:46:04 -0400

    chapter 2 of the rust programming language book

---
+++ src/chapter_2/guess.rs
@@ -0,0 +1,38 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
+pub fn guessing_game() {
+    println!("Guess the number!");
+
+    let num: u32 = rand::thread_rng().gen_range(1, 101);
+
+    loop {
+        println!("Enter a guess!");
+
+        let mut guess: String = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to readline");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Guess a number bozo!");
+                continue;
+            }
+        };
+
+        println!("You guessed {}", guess);
+
+        match guess.cmp(&num) {
+            Ordering::Less => println!("Too Small!"),
+            Ordering::Greater => println!("Too Big!"),
+            Ordering::Equal => {
+                println!("Well Done!");
+                break;
+            }
+        }
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,6 @@
+use crate::chapter_2::guess::guessing_game;
+mod chapter_2;
+
+fn main() {
+    guessing_game();
+}
