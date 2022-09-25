acec3c6c0ff7695527c969737e285e57a401e05f
ijs32 <iansilber32@gmail.com>
2022-09-24 21:49:09 -0400

    changing way I bring functions into scope to match the rust best practices

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,17 @@
-// use crate::chapter_2::guess::guessing_game;
-// use crate::chapter_3::basics::variables;
-// use crate::chapter_4::ownership::ownership;
-// use crate::chapter_5::structs::structs;
-use crate::chapter_6::enums::enums;
+// use crate::chapter_2::guess;
+// use crate::chapter_3::basics;
+// use crate::chapter_4::ownership;
+// use crate::chapter_5::structs;
+use crate::chapter_6::enums;
 mod chapter_2;
 mod chapter_3;
 mod chapter_4;
 mod chapter_5;
 mod chapter_6;
 fn main() {
-    // guessing_game();
-    // variables();
-    // ownership();
-    // structs();
-    enums();
+    // guess::guessing_game();
+    // basics::variables();
+    // ownership::ownership();
+    // structs::structs();
+    enums::enums();
 }
